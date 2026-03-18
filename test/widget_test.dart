af4a79686335c2d750a5a67e8ee6e533b2fdae07
mohmmad5921-ux <mohmmad5921@gmail.com>
2026-03-18 12:39:51 +0100

    Delete test directory
    Delete assets directory

--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:syrian_digital_id/main.dart';
-
-void main() {
-  testWidgets('App smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const SyrianDigitalIdApp());
-    expect(find.text('الهوية الرقمية السورية'), findsOneWidget);
-  });
-}
